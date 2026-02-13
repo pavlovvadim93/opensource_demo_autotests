a442228e3122aa6a38af68accfebd5046458219c
pavlovvadim93 <pavlov.vadim.93@mail.ru>
2026-02-13 10:09:32 +0200

    test: fix parametrization and handle None in error message

--- tests/test_multiple_invalid_passwords.py
+++ tests/test_multiple_invalid_passwords.py
@@ -4,9 +4,12 @@ import allure
 
 @allure.feature("Авторизация")
 @allure.story("Негативный сценарий")
-@pytest.mark.parametrize("password", ["wrong", "12345", "", "   "])
+@pytest.mark.parametrize("password", ["wrong", "12345", "qadefef", "2313dd"])
 def test_multiple_invalid_passwords(browser, password):
     login_page = LoginPage(browser)
     login_page.open()
     login_page.login("Admin", password)
-    assert "Invalid credentials" in login_page.get_error_message()
+    
+    error_text = login_page.get_error_message()
+    assert error_text is not None, "Сообщение об ошибке не появилось"
+    assert "Invalid credentials" in error_text
